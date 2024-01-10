28e3788ef8da964ab9fe9cb0264dfcb83f61b597
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-01-10 23:35:29 +0100

    feat: slider and tabs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // бургер-меню
+  // burger menu
+
   const menuBurgerBtn = document.querySelector('.menu-burger-btn'),
     menuBurger = document.querySelector('.nav-list'),
     menuBurgerItem = document.querySelectorAll('.nav-item');
@@ -23,18 +24,24 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // слайдер
+  // slider
   let position = 1;
-  const slidesToShow = 3;
+  slidesToShow = 1;
+  if (window.innerWidth >= 1024) {
+    slidesToShow = 2;
+  }
+  if (window.innerWidth >= 1440) {
+    slidesToShow = 3;
+  }
+
   const slidesToScroll = 1;
   const container = document.querySelector('.slider-container');
   const track = document.querySelector('.slider-items');
   const items = document.querySelectorAll('.slider-item');
-  const btnPrev = document.querySelector('.btn-prev');
-  const btnMid = document.querySelector('.btn-mid');
-  const btnNext = document.querySelector('.btn-next');
+  const btnItem = document.querySelector('.slider-pagination-bg');
+  const btnContainer = document.querySelector('.slider-pagination-container');
 
-  const showSlides = (n) => {
+  const showSlides = (btns, n) => {
     if (n > items.length - slidesToShow + 1) {
       position = items.length - slidesToShow + 1;
     }
@@ -42,32 +49,75 @@ window.addEventListener('DOMContentLoaded', () => {
       position = 1;
     }
 
-    items.forEach((item) => {
-      item.style.display = 'block';
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
     });
 
-    items.forEach((item, n) => {
-      if (n < position - 1 || n > position + slidesToShow - 1) {
-        item.style.display = 'none';
-      }
+    btns[n - 1].classList.add('active');
+    track.style.left = -(position - 1) * 475 + 'px';
+  };
+
+  for (
+    let i = 0;
+    i < Math.round((items.length - slidesToShow) / slidesToScroll);
+    i++
+  ) {
+    const newBtnContainer = btnItem.cloneNode(true);
+    const newBtn = newBtnContainer.querySelector('.slider-pagination');
+    newBtn.classList.remove('active');
+    btnContainer.innerHTML += newBtnContainer.outerHTML;
+  }
+
+  const btns = document.querySelectorAll('.slider-pagination');
+  btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      showSlides(btns, (position = index + 1));
     });
-    // move items to the left
-    // position dynamically changes
-    // track.style.position = 'relative';
-    // track.style.left = -(position - 1) * 100 + '%';
-
-    // items[position - 1].style.position = 'relative';
-  };
-
-  const nextSlide = (n) => {
-    showSlides((position += n));
-  };
-
-  btnPrev.addEventListener('click', () => {
-    nextSlide(-1);
   });
 
-  btnNext.addEventListener('click', () => {
-    nextSlide(1);
+  // tabs
+  const tabsInput = document.querySelectorAll('.season-choice-input'),
+    tabsContent = document.querySelectorAll('.book-card-full'),
+    tabsParent = document.querySelector('.season-choice');
+
+  const hideTabContent = () => {
+    tabsContent.forEach((item) => {
+      item.style.display = 'none';
+      item.parentNode.style.marginBottom = '0';
+    });
+
+    tabsInput.forEach((item) => {
+      item.removeAttribute('checked');
+    });
+  };
+
+  const showTabContent = (i = 'winter') => {
+    tabsContent.forEach((item) => {
+      if (item.classList.contains(i)) {
+        item.style.display = 'block';
+        item.parentNode.style.marginBottom = '40px';
+      }
+    });
+    tabsInput.forEach((item) => {
+      if (item.id === i) {
+        item.setAttribute('checked', 'checked');
+      }
+    });
+  };
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('season-choice-input')) {
+      tabsInput.forEach((item) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(item.id);
+        }
+      });
+    }
   });
 });
